5b7fee36398a285ee3f4845ea782feab6b431c52
temposh016 <timon3529@gmail.com>
2025-12-05 16:02:27 +0500

    go go go 2

--- src/main/java/com/example/midmid/Config/MinioConfig.java
+++ src/main/java/com/example/midmid/Config/MinioConfig.java
@@ -1,5 +1,30 @@
 package com.example.midmid.Config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 public class MinioConfig {
 
+    @Configuration
+    public class MinioConfig {
+        @Value("${minio.url}")
+        private String url;
+
+        @Value("${minio.access}")
+        private String access;
+
+        @Value("${minio.secret}")
+        private String secret;
+
+        @Bean
+        public MinioClient minioClient(){
+            return MinioClient.builder()
+                    .endpoint(url)
+                    .credentials(access, secret)
+                    .build();
+
+        }
+
+    }
 }
--- src/main/java/com/example/midmid/Controller/FileController.java
+++ src/main/java/com/example/midmid/Controller/FileController.java
@@ -1,4 +1,23 @@
 package com.example.midmid.Controller;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/files")
 public class FileController {
+
+    private final MinioService minioService;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) throws Exception {
+        minioService.upload(file);
+        return "файл жуктелды";
+    }
+
 }
